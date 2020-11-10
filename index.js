f0511904352b0d8fc028e71d5fd3d25d1e9b33f2
Varun <varunsridharan23@gmail.com>
2020-11-10 15:31:42 +0530

    Updated

--- index.js
+++ index.js
@@ -15,9 +15,7 @@ const exec = ( cmd, args = [] ) => new Promise( ( resolve, reject ) => {
 } );
 
 const main = async() => {
-	await exec( 'bash', [ path.join( __dirname, './entrypoint.sh' ) ] ).then( function() {
-		console.log( arguments );
-	} ).catch( function( err ) {
+	await exec( 'bash', [ path.join( __dirname, './entrypoint.sh' ) ] ).catch( function( err ) {
 		console.log( err );
 	} );
 };
