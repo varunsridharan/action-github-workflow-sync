426fe7fa8dd20bc779c302f6b494400bc56f31c3
Varun <varunsridharan23@gmail.com>
2020-11-13 12:22:32 +0530

    Updated

--- dist/index.js
+++ dist/index.js
@@ -1640,47 +1640,41 @@ module.exports     = {
 
 /***/ }),
 
-/***/ 904:
-/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
-
-const gh_core = __webpack_require__( 186 );
-
-module.exports = ( log, before = '' ) => gh_core.error( `${before}🛑️  ${log}` );
-
-
-/***/ }),
-
 /***/ 39:
 /***/ ((module, __unused_webpack_exports, __webpack_require__) => {
 
 const gh_core = __webpack_require__( 186 );
 const log     = ( log ) => gh_core.info( `${log}` );
+const style   = __webpack_require__( 68 );
 
-log.error   = __webpack_require__( 904 );
-log.success = __webpack_require__( 277 );
-log.warning = __webpack_require__( 37 );
+log.success = ( log, before = '' ) => gh_core.info( `${before}✔  ${log}` );
+log.warning = ( log, before = '' ) => gh_core.warning( `${before}⚠️ ${log}` );
+log.error   = ( log, before = '' ) => gh_core.error( `${before}🛑️  ${log}` );
+log.warn    = ( log, before = '' ) => gh_core.info( `${before}⚠️ ${log}` );
+log.err     = ( log, before = '' ) => gh_core.info( `${before}🛑️  ${log}` );
+/**
+ * Colored Logs
+ */
+
+log.black         = ( data ) => log( `${style.black.open}${data}${style.black.close}` );
+log.red           = ( data ) => log( `${style.red.open}${data}${style.red.close}` );
+log.green         = ( data ) => log( `${style.green.open}${data}${style.green.close}` );
+log.yellow        = ( data ) => log( `${style.yellow.open}${data}${style.yellow.close}` );
+log.blue          = ( data ) => log( `${style.blue.open}${data}${style.blue.close}` );
+log.magenta       = ( data ) => log( `${style.magenta.open}${data}${style.magenta.close}` );
+log.cyan          = ( data ) => log( `${style.cyan.open}${data}${style.cyan.close}` );
+log.white         = ( data ) => log( `${style.white.open}${data}${style.white.close}` );
+log.gray          = ( data ) => log( `${style.gray.open}${data}${style.gray.close}` );
+log.redBright     = ( data ) => log( `${style.redBright.open}${data}${style.redBright.close}` );
+log.greenBright   = ( data ) => log( `${style.greenBright.open}${data}${style.greenBright.close}` );
+log.yellowBright  = ( data ) => log( `${style.yellowBright.open}${data}${style.yellowBright.close}` );
+log.blueBright    = ( data ) => log( `${style.blueBright.open}${data}${style.blueBright.close}` );
+log.magentaBright = ( data ) => log( `${style.magentaBright.open}${data}${style.magentaBright.close}` );
+log.cyanBright    = ( data ) => log( `${style.cyanBright.open}${data}${style.cyanBright.close}` );
+log.whiteBright   = ( data ) => log( `${style.whiteBright.open}${data}${style.whiteBright.close}` );
+
 
 module.exports = log;
-
-
-/***/ }),
-
-/***/ 277:
-/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
-
-const gh_core = __webpack_require__( 186 );
-
-module.exports = ( log, before = '' ) => gh_core.error( `${before}✔  ${log}` );
-
-
-/***/ }),
-
-/***/ 37:
-/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
-
-const gh_core = __webpack_require__( 186 );
-
-module.exports = ( log, before = '' ) => gh_core.warning( `${before}⚠️ ${log}` );
 
 
 /***/ }),
