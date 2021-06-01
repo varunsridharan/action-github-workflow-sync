cd4f0dbfb282260c1fcad62bd31a6a7c1d00d410
afirth <afirth@users.noreply.github.com>
2021-06-01 18:43:19 +0200

    npm install

--- dist/index.js
+++ dist/index.js
@@ -9009,7 +9009,7 @@ async function run() {
 					let cp_options    = ( is_dir ) ? { recursive: true, force: true } : {},
 						iscopied      = true,
 						dest_basepath = toolkit.path.dirname( `${local_path}${workflow_file.dest}` ),
-						copy_source   = ( is_dir ) ? `${toolkit.path.trailingslashit( source_path )}.` : source_path;
+						copy_source   = source_path;
 
 					toolkit.log.success( `${relative_path} => ${workflow_file.dest}`, '	' );
 
@@ -9085,6 +9085,7 @@ async function run() {
 }
 
 run();
+
 
 /***/ }),
 
